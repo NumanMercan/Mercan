dedca08100602c7ddadc40a5b3b1f9a61d08d455
Numan <57049173+NumanMercan@users.noreply.github.com>
2020-10-22 01:59:52 +0300

    finding BigCount from Java

---
+++ BTKBuyukSayi.java
@@ -0,0 +1,18 @@
+public class BTKBuyukSayi {
+
+    public static void main(String[] args) {
+        int sayi1=43 , sayi2=25 , sayi3=2;
+         if(sayi1 > sayi2){
+              if (sayi1 > sayi3){
+                  System.out.println("En buyuk sayi1");
+              }
+              else {
+                  System.out.println("En buyuk sayi3");
+              }
+         }
+          else {
+             System.out.println("Sayi 2 en buyuk");
+         }
+
+    }
+}
